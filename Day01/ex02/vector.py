28fa16d990f3cab45a031255acc8308f90b086a7
moha08-learn <mohawatch101@gmail.com>
2023-02-18 21:23:44 +0100

    implemeting transpose member function

--- Day01/ex02/vector.py
+++ Day01/ex02/vector.py
@@ -1,9 +1,10 @@
 from typing import List
 from typing import Tuple
+import operator
 
 
 class Vector:
-    def __init__(self,values:List[List[float]], shape:Tuple(int,int)):
+    def __init__(self,values:List[List[float]], shape:Tuple[int,int]):
         self.values = values
         self.shape = shape
     def dot(self, vect):
@@ -13,15 +14,32 @@ class Vector:
         if self.shape != vect.shape:
             print(f"The shape of the vectors is diffrent you cannot perform multiplication")
             return
-        product = Vector()
-        for v1, v2 in zip(self.values[0], vect.values[0]):
-            product.values[0].append(v1 * v2)
+        product = Vector([],(self.shape[0], self.shape[1]))
+        for v1, v2 in zip(self.values, vect.values):
+            product.values.append(v1 * v2)
         return product
+    def T(self):
+        if self.shape[0] == 1:
+            trans = Vector([], (self.shape[1], self.shape[0]))
+            for i in range(self.shape[1]):
+                trans.values.append([self.values[i]])
+            return trans
+        else:
+            trans = Vector([],(self.shape[1], self.shape[0]))
+            for i in range(self.shape[0]):
+                trans.values.append(self.values[i][0])
+            return trans
 
-a = Vector([[1.0, 2.0, 3.0]], (1,3))
+vect1 = Vector([[1.0], [2.0], [3.0]], (3,1))
 
-b = Vector([[2.0, 2.0, 2.0]], (1,3))
+# vect2 = Vector([2.0, 2.0, 2.0], (3,1))
 
-c = a.dot(b)
+# vect2 = operator.mul(vect2, 5)
 
-print(c.values)
+# vect3 = vect1.T()
+
+# print(vect3.shape)
+
+# vect3 = vect1.dot(vect2)
+
+# print(vect3.values)
