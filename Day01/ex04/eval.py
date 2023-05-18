d9d642c4d5ab97c7cac8c8e49ffa972cbb8bd32d
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-05-18 20:38:11 +0100

    finnish eval problem

---
+++ Day01/ex04/eval.py
@@ -0,0 +1,25 @@
+import typing
+
+class Evaluator:
+    def zip_evaluate(coefs: list, words: list) -> int:
+        if len(coefs) != len(words):
+            return -1
+        if not all(isinstance(coef, float) for coef in coefs):
+            return -1
+        if not all(isinstance(word, str) for word in words):
+            return -1
+        sum = 0
+        for item in zip(coefs, words):
+            sum += (len(item[1]) * item[0])
+        return sum
+    def enumerate_evaluate(coefs: list, words: list) -> int:
+        if len(coefs) != len(words):
+            return -1
+        if not all(isinstance(coef, float) for coef in coefs):
+            return -1
+        if not all(isinstance(word, str) for word in words):
+            return -1
+        sum = 0
+        for item in enumerate(words):
+            sum += (len(item[1]) * coefs[item[0]])
+        return sum
